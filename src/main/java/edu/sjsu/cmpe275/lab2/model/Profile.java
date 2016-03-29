7c3d671ca39891f4c8648288a297263a8561dbe9
lazylad91 <parteek.makkar@sjsu.edu>
2016-03-29 12:03:49 -0700

    first commit

---
+++ src/main/java/edu/sjsu/cmpe275/lab2/model/Profile.java
@@ -0,0 +1,99 @@
+package edu.sjsu.cmpe275.lab2.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "profile")
+public class Profile
+{
+	@Id
+	@GeneratedValue
+	private String id;
+	@Column(name = "first_name")
+	private String firstname;
+	@Column(name = "last_name")
+	private String lastname;
+	@Column(name = "email")
+	private String email;
+	@Column(name = "address")
+	private String address;
+	@Column(name = "organization")
+	private String organization;
+	@Column(name = "about_me")
+	private String aboutMyself;
+
+	public String getId()
+	{
+		return id;
+	}
+
+	public void setId(String id)
+	{
+		this.id = id;
+	}
+
+	public String getFirstname()
+	{
+		return firstname;
+	}
+
+	public void setFirstname(String firstname)
+	{
+		this.firstname = firstname;
+	}
+
+	public String getLastname()
+	{
+		return lastname;
+	}
+
+	public void setLastname(String lastname)
+	{
+		this.lastname = lastname;
+	}
+
+	public String getEmail()
+	{
+		return email;
+	}
+
+	public void setEmail(String email)
+	{
+		this.email = email;
+	}
+
+	public String getAddress()
+	{
+		return address;
+	}
+
+	public void setAddress(String address)
+	{
+		this.address = address;
+	}
+
+	public String getOrganization()
+	{
+		return organization;
+	}
+
+	public void setOrganization(String organization)
+	{
+		this.organization = organization;
+	}
+
+	public String getAboutMyself()
+	{
+		return aboutMyself;
+	}
+
+	public void setAboutMyself(String aboutMyself)
+	{
+		this.aboutMyself = aboutMyself;
+	}
+
+}
